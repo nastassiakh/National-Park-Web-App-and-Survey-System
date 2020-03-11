ea63b2257b22794687f67bd2c52f49ce3c716a49
Ryan Quigley <ryan.quigley32@outlook.com>
2020-03-11 11:51:13 -0400

    update

--- src/main/java/com/techelevator/npgeek/JdbcParkDao.java
+++ src/main/java/com/techelevator/npgeek/JdbcParkDao.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
-import com.techelevator.model.Park;
 import com.techelevator.ssg.model.forum.ForumPost;
 
 public class JdbcParkDao implements ParkDao {
