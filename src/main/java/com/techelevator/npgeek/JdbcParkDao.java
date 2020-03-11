2c45ced357ade0ebd88e5b03af0c8b9966d8fcf3
Archana Rajmohan <archanarajmohan@gmail.com>
2020-03-11 11:17:52 -0400

    models weather and park

---
+++ src/main/java/com/techelevator/npgeek/JdbcParkDao.java
@@ -0,0 +1,50 @@
+package com.techelevator.npgeek;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import com.techelevator.model.Park;
+import com.techelevator.ssg.model.forum.ForumPost;
+
+public class JdbcParkDao implements ParkDao {
+	
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public JdbcParkDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	@Override
+	public List<Park> getAllParks() {
+		String sqlGetAllParks = "SELECT * FROM park";
+		SqlRowSet result =	jdbcTemplate.queryForRowSet(sqlGetAllParks);
+		while (result.next()) {
+			ForumPost Park = new ForumPost();
+			post.setId(result.getLong("id"));
+			post.setUsername(results.getString("username"));
+			post.setSubject(results.getString("subject"));
+			post.setMessage(results.getString("message"));
+//			post.setDatePosted(results.getTimestamp("post_date").toLocalDateTime());
+			allPosts.add(post);
+		}
+		return Park;
+		
+		
+	}
+	private Park mapRowToPark(SqlRowSet result) {
+
+		Park newPark = new Park();
+		newPark.setParkId(result.getLong("park_id"));
+		newPark
+		
+		return newPark;
+
+	}
+	
+}
---
+++ src/main/java/com/techelevator/npgeek/Park.java
@@ -0,0 +1,113 @@
+package com.techelevator.npgeek;
+
+public class Park {
+	private String parkCode;
+	private String parkName;
+	private String state;
+	private Long acreage;
+	private Long elevation;
+	private float milesOfTrail;
+	private Long numberOfCampsites;
+	private String climate;
+	private Long yearFounded;
+	private Long annualVisitorCount;
+	private String quote;
+	private String quoteSource;
+	private String description;
+	private Long entryFee;
+	private Long numberOfAnimalSpecies;
+	public String getParkCode() {
+		return parkCode;
+	}
+	public void setParkCode(String parkCode) {
+		this.parkCode = parkCode;
+	}
+	public String getParkName() {
+		return parkName;
+	}
+	public void setParkName(String parkName) {
+		this.parkName = parkName;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Long getAcreage() {
+		return acreage;
+	}
+	public void setAcreage(Long acreage) {
+		this.acreage = acreage;
+	}
+	public Long getElevation() {
+		return elevation;
+	}
+	public void setElevation(Long elevation) {
+		this.elevation = elevation;
+	}
+	public float getMilesOfTrail() {
+		return milesOfTrail;
+	}
+	public void setMilesOfTrail(float milesOfTrail) {
+		this.milesOfTrail = milesOfTrail;
+	}
+	public Long getNumberOfCampsites() {
+		return numberOfCampsites;
+	}
+	public void setNumberOfCampsites(Long numberOfCampsites) {
+		this.numberOfCampsites = numberOfCampsites;
+	}
+	public String getClimate() {
+		return climate;
+	}
+	public void setClimate(String climate) {
+		this.climate = climate;
+	}
+	public Long getYearFounded() {
+		return yearFounded;
+	}
+	public void setYearFounded(Long yearFounded) {
+		this.yearFounded = yearFounded;
+	}
+	public Long getAnnualVisitorCount() {
+		return annualVisitorCount;
+	}
+	public void setAnnualVisitorCount(Long annualVisitorCount) {
+		this.annualVisitorCount = annualVisitorCount;
+	}
+	public String getQuote() {
+		return quote;
+	}
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+	public String getQuoteSource() {
+		return quoteSource;
+	}
+	public void setQuoteSource(String quoteSource) {
+		this.quoteSource = quoteSource;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getEntryFee() {
+		return entryFee;
+	}
+	public void setEntryFee(Long entryFee) {
+		this.entryFee = entryFee;
+	}
+	public Long getNumberOfAnimalSpecies() {
+		return numberOfAnimalSpecies;
+	}
+	public void setNumberOfAnimalSpecies(Long numberOfAnimalSpecies) {
+		this.numberOfAnimalSpecies = numberOfAnimalSpecies;
+	}
+	
+	
+	
+
+}
