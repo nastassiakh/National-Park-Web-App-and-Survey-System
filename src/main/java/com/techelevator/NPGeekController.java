97fa7b56bce3275cf72c5d34592d8efa50d2c472
Ryan Quigley <ryan.quigley32@outlook.com>
2020-03-14 15:27:06 -0400

    update

--- src/main/java/com/techelevator/NPGeekController.java
+++ src/main/java/com/techelevator/NPGeekController.java
@@ -44,7 +44,12 @@ public class NPGeekController {
 
 	@RequestMapping("/parkdetail")
 	public String displayParkdetail(ModelMap map, @RequestParam String parkCode, HttpSession session) {
-		List<Weather> forecast = weatherDao.getFiveDayForecast(parkCode, (String) session.getAttribute("tempScale"));
+		// Get either F or C from session
+		String tempType = (String) session.getAttribute("tempScale");
+		// run getFiveDayForecast and get correct weather number
+		List<Weather> forecast = weatherDao.getFiveDayForecast(parkCode, tempType);
+		
+		
 		map.put("forecast", forecast);
 		Park park = parkDao.getParkByCode(parkCode);
 		map.put("park", park);
@@ -52,11 +57,13 @@ public class NPGeekController {
 		map.put("parkpic", pic);
 		session.setAttribute("parkCode", parkCode);
 		map.put("detailURL", "parkdetail?parkCode=" + parkCode);
+		
 		return "parkdetail";
 	}
 
 	@RequestMapping(path = "/parkdetail", method = RequestMethod.POST)
 	public String getTemp(HttpSession session, @RequestParam String tempScale, @RequestParam String detailURL) {
+		//pull tempScale out of dropdown menu and put it into the session
 		session.setAttribute("tempScale", tempScale);
 		return "redirect:/" + detailURL;
 	}
